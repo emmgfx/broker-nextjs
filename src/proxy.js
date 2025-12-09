9249fd555135343d65a9ce28de567ddd6571b691
Josep Viciana <emmgfx@gmail.com>
2025-12-09 11:51:21 +0100

    middleware -> proxy

--- src/proxy.js
+++ src/proxy.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-export function middleware(request) {
+export function proxy(request) {
   const basicAuth = request.headers.get("authorization");
   if (basicAuth) {
     const authValue = basicAuth.split(" ")[1];
